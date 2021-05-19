95e5921cd91318c880729bfefbc5ce6246f57d2b
Elisama Talita Cabral Santana <82914088+TalitaCbral@users.noreply.github.com>
2021-05-19 19:23:55 -0300

    ok

--- Aula 12 exercicios/1.py
+++ Aula 12 exercicios/1.py
@@ -1,4 +1,4 @@
-#1.	Exercício Treino - Crie um dicionário em que suas chaves serão os números 1, 4, 5, 6, 7, e 9 
-# (que podem ser armazenados em uma lista) e seus valores correspondentes
-# aos quadrados desses números. 
-
+#1.- Exercício Treino - Crie um dicionário em que suas chaves serão os números 
+#1, 4, 5, 6, 7, e 9 (que podem ser armazenados em uma lista) e seus valores
+#correspondentes aos quadrados desses números. 
+lista = [1, 4, 5, 6, 7, 9]
